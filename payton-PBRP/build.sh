79ef8d612a2d791a0f938c4a4f333215840766c5
YukoSky <daffetyxd@gmail.com>
2020-07-30 19:52:31 -0300

    (BUILD) payton-PBRP

---
+++ payton-PBRP/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Sync
+telegram -M "PBRP - Moto X4: Sync Device (Dependencies) Tree started"
+SYNC_START=$(date +"%s")
+
+. build/envsetup.sh
+lunch omni_payton-eng
+rm -rf .repo
+
+SYNC_END=$(date +"%s")
+SYNC_DIFF=$((SYNC_END - SYNC_START))
+telegram -M "PBRP - Moto X4: Device Tree Sync (Dependencies) completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+
+# Build
+telegram -M "PBRP - Moto X4: Building Recovery started"
+SYNC_START=$(date +"%s")
+
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL=C
+make -j4 recoveryimage
+
+SYNC_END=$(date +"%s")
+SYNC_DIFF=$((SYNC_END - SYNC_START))
+telegram -M "PBRP - Moto X4: Building completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+
+# Output
+telegram -f out/target/product/payton/PBRP-payton-*-*-*-UNOFFICIAL.zip "[PBRP Recovery] [Unofficial] - Moto X4"
