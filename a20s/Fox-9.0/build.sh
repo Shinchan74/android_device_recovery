7e39a7d86119d096ed864f6d0ac397962b3406ef
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-11-05 12:53:18 -0300

    [OFOX] A20s
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

---
+++ a20s/Fox-9.0/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+source ~/tmp/config.sh
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+HOSTNAME=´cat /etc/hostname >/dev/null´
+distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+
+~/tmp/telegram -M "⚒ ***Recovery***: [OrangeFox](https://gitlab.com/OrangeFox) (R11.0)
+📱 ***Device***: Samsung Galaxy A20s
+🖥 ***Machine Host***: $HOSTNAME (OS: $distro)
+⚙️ ***Device codename***: a20s
+
+📍 ***Note***: Building Recovery started"
+SYNC_START=$(date +"%s")
+
+cd "$THIS_DIR/OrangeFox"
+
+./build_ofox.sh
+
+SYNC_END=$(date +"%s")
+SYNC_DIFF=$((SYNC_END - SYNC_START))
+if [ -f "$THIS_DIR/OrangeFox/out/target/product/a20s/OrangeFox-R11.0-Beta-a20s.zip" ]; then
+~/tmp/telegram -M "OrangeFox (R11.0) - A20s (a20s)
+***📦 Building completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds***"
+   # Output
+~/tmp/telegram -M "📦 ***Recovery***: [OrangeFox](https://gitlab.com/OrangeFox) (R11.0)
+📱 ***Device***: Samsung Galaxy A20s
+⚙️ ***Device codename***: a20s
+🎈 ***Output***: Recovery ZIP Flasheable
+
+📍 ***Tags***: #a20s #ofr #r11 #beta"
+~/tmp/telegram -f "$THIS_DIR/OrangeFox/out/target/product/a20/OrangeFox-R11.0-Beta-a20s.zip" ""
+
+   # Output for: Image
+~/tmp/telegram -M "📦 ***Recovery***: [OrangeFox](https://gitlab.com/OrangeFox) (R11.0)
+📱 ***Device***: Samsung Galaxy A20s
+⚙️ ***Device codename***: a20s
+🎈 ***Output***: Recovery Image
+
+📍 ***Tags***: #a20s #ofr #r11 #beta"
+~/tmp/telegram -f "$THIS_DIR/OrangeFox/out/target/product/a20s/recovery.img" ""
+else
+   # Error in build
+~/tmp/telegram -M "⚒ ***Recovery***: [OrangeFox](https://gitlab.com/OrangeFox) (R11.0)
+📱 ***Device***: Samsung Galaxy A20s
+🖥 ***Machine Host***: $HOSTNAME (OS: $distro)
+⚙️ ***Device codename***: a20s
+
+❌ ***Note***: Building completed unsuccessfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+fi
+
+curl --data parse_mode=HTML --data chat_id=$CHATS --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TOKEN/sendSticker
